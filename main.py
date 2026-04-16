15f911b1834a6414e69c9f10dbe742f81280490a
aaaaw3668-web <aaaaw3668@gmail.com>
2026-04-16 18:50:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import atexit
 # Настройки
 TELEGRAM_BOT_TOKEN = '7446722367:AAFfl-bNGvYiU6_GpNsFeRmo2ZNZMJRx47I'
 OI_THRESHOLD = 2.5
-PRICE_INCREASE_THRESHOLD = 10  # Порог для роста цены
+PRICE_INCREASE_THRESHOLD = 2  # Порог для роста цены
 PRICE_DECREASE_THRESHOLD = -10
 
 # Порог для падения цены
