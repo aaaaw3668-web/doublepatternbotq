bef55deeaf3fb3e1cb6f4052a7ea7bc25813c64e
aaaaw3668-web <aaaaw3668@gmail.com>
2026-07-30 13:33:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ if not TELEGRAM_BOT_TOKEN:
     print("✗ Ошибка: TELEGRAM_BOT_TOKEN не найден в переменных окружения!")
     exit(1)
 
-OI_THRESHOLD = 3
-PRICE_INCREASE_THRESHOLD = 5   #Порог для роста цены
+OI_THRESHOLD = 500
+PRICE_INCREASE_THRESHOLD = 3   #Порог для роста цены
 PRICE_DECREASE_THRESHOLD = -30     # Порог для падения цены
 TIME_WINDOW = 60 * 5
 DAILY_ALERT_LIMIT = 100              # Лимит уведомлений на одну монету в день
