cf70cffe0cfe02aeba5d5f1b805bbe8b36c34262
aaaaw3668-web <aaaaw3668@gmail.com>
2026-09-02 16:42:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if not TELEGRAM_BOT_TOKEN:
     exit(1)
 
 OI_THRESHOLD = 500
-PRICE_INCREASE_THRESHOLD = 2    # Порог для роста цены
+PRICE_INCREASE_THRESHOLD = 5    # Порог для роста цены
 PRICE_DECREASE_THRESHOLD = -30     # Порог для падения цены
 TIME_WINDOW = 60 * 5
 DAILY_ALERT_LIMIT = 100             # Лимит уведомлений на одну монету в день
